f1adc4b09d776dcb970b70d7f6831ec5ca7be3f0
Tanvir hossain <tanvirhossain4393@gamil.com>
2023-04-11 17:15:13 +0600

    header complete

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -4,18 +4,28 @@ import './Header.css'
 
 const Header = () => {
     return (
-        <div className='header-container'>
-            <div>
-                <h3>IT Center</h3>
+        <div>
+            <div className='header-container'>
+                <div>
+                    <h3>IT Center</h3>
+                </div>
+                <nav>
+                    <Link to="/">Home</Link>
+                    <Link to="/statistics">Statistics</Link>
+                    <Link to="/jobs">Applied Jobs</Link>
+                    <Link to="/blogs">Blogs</Link>
+                </nav>
+                <div>
+                    <button className='btn-apply'>Apply</button>
+                </div>
             </div>
-            <nav>
-                <Link to="/">Home</Link>
-                <Link to="/statistics">Statistics</Link>
-                <Link to="/jobs">Applied Jobs</Link>
-                <Link to="/blogs">Blogs</Link>
-            </nav>
-            <div>
-                <button className='btn-apply'>Apply</button>
+            <div className='header-second-section'>
+                <div className='header-title'>
+                    <h1>Discover Your <br /> Next Career <span className='title-color'> Opportunity</span></h1>
+                </div>
+                <div>
+                    <img src="https://thumbs.dreamstime.com/b/smiling-businessman-office-colleagues-background-smiling-businessman-office-colleagues-background-101628040.jpg" alt="" />
+                </div>
             </div>
         </div>
     );
