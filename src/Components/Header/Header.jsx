57ff4c67662a84b32a68ef19bab23a39e06edeb8
Tanvir hossain <tanvirhossain4393@gamil.com>
2023-04-11 16:52:28 +0600

    header nav items added and btn created

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,14 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './Header.css'
 
 const Header = () => {
     return (
-        <nav>
-            <Link to ="/">Home</Link>
-            <Link to ="/statistics">Statistics</Link>
-            <Link to ="/jobs">Applied Jobs</Link>
-            <Link to ="/blogs">Blogs</Link>
-        </nav>
+        <div className='header-container'>
+            <div>
+                <h3>IT Center</h3>
+            </div>
+            <nav>
+                <Link to="/">Home</Link>
+                <Link to="/statistics">Statistics</Link>
+                <Link to="/jobs">Applied Jobs</Link>
+                <Link to="/blogs">Blogs</Link>
+            </nav>
+            <div>
+                <button className='btn-apply'>Apply</button>
+            </div>
+        </div>
     );
 };
 
