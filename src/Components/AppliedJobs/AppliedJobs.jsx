c9f2dd169e9df3075a12e145e082cb2ce340b604
Tanvir hossain <tanvirhossain4393@gamil.com>
2023-04-14 14:37:21 +0600

    filter job data completed

--- src/Components/AppliedJobs/AppliedJobs.jsx
+++ src/Components/AppliedJobs/AppliedJobs.jsx
@@ -15,17 +15,30 @@ const AppliedJobs = () => {
         }
     }, [])
 
+    const handleFilter = (event) => {
+        const value =event.target.value;
+        const filterData = jobs.filter(item => item.jobType == value);
+        setFilterJobs(filterData)
+    }
+
     return (
         <div>
             <h2 className='text-3xl font-bold text-center my-24'>Applied Jobs</h2>
+            <div className='w-3/4 flex justify-end mx-auto mb-3'>
+                <select onChange={handleFilter} className="select select-bordered w-full max-w-xs">
+                    <option disabled selected>Filter Job</option>
+                    <option value="Full-time">Full Time</option>
+                    <option value="Remote">Remote</option>
+                </select>
+            </div>
             {
-                filterJobs.length > 0 ? filterJobs ?.map(job =><div> 
-                <StoredData job={job}></StoredData>
+                filterJobs.length > 0 ? filterJobs?.map(job => <div>
+                    <StoredData job={job}></StoredData>
                 </div>
-                 ) 
-                 :
-                 <h1 className='text-4xl text-center font-extrabold text-orange-700'>No Application found!!!</h1>
-        }
+                )
+                    :
+                    <h1 className='text-4xl text-center font-extrabold text-orange-700'>No Application found!!!</h1>
+            }
 
         </div>
     );
