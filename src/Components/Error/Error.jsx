86199f7e165d50a73b5079eacac04052bd4eec0f
Tanvir hossain <tanvirhossain4393@gamil.com>
2023-04-14 16:43:59 +0600

    error page recreated and designed by tailwind

--- src/Components/Error/Error.jsx
+++ src/Components/Error/Error.jsx
@@ -5,8 +5,8 @@ import {faCircleExclamation} from '@fortawesome/free-solid-svg-icons'
 
 const Error = () => {
     return (
-        <div className='error'>
-            <h2>404 </h2>
+        <div className='w-80 text-center mx-auto mt-52 text-4xl font-extrabold  '>
+            <h2 className='text-red-500 text-8xl mb-8'>404 </h2>
             <h3>Not Found <FontAwesomeIcon icon={faCircleExclamation} /></h3>
         </div>
     );
